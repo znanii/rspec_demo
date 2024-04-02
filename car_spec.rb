a67336bc6a859f50d071fa9677074aa7cbccf2b6
zhanii <naborznanii@gmail.com>
2024-04-02 11:10:19 -0700

    Add test for car (with bug)

--- car_spec.rb
+++ car_spec.rb
@@ -1,8 +1,20 @@
-require './car.rb'
+require './car'
 
 describe Car do
-  
+
+it "must return range" do
+  #arrange
+  car = Car.new
+
+  #act
+
+  car.add_fuel 10
+
+  #assert
+
+  expect(car.range).to eq 200
+end
 
 
-  
+
 end
